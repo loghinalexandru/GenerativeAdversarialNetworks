ae5d92c616bb7b26956032f82c0e08f5e6779b0f
Loghin Alexandru <loghinalexandru61@gmail.com>
2021-05-21 14:10:04 +0300

    Update autoencoder.py

--- src/ffhq/autoencoder.py
+++ src/ffhq/autoencoder.py
@@ -1,21 +1,33 @@
-import tensorflow as tf
 import os
-import keras
+import tensorflow as tf
+from tensorflow import keras
 import numpy as np
 import matplotlib as mathplt
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from batchup import data_source
-from keras.models import Sequential
-from keras.layers import InputLayer, Dense , Dropout, LeakyReLU, BatchNormalization, Reshape, UpSampling2D, Conv2D, Flatten, Activation
-from keras import activations
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import InputLayer, Dense , Dropout, LeakyReLU, BatchNormalization, Reshape, Conv2DTranspose, Conv2D, UpSampling2D, Flatten, Activation, MaxPool2D
+from tensorflow.keras.initializers import RandomNormal
 
-latent_dim = 2
-batch_size = 32
-epochs = 10
+batch_size = 16
+max_input_size = 10000
+epochs = 1000
+images_path = "./dataset"
+
+def load_data():
+    input_data = []
+    for entry in os.listdir(images_path):
+        if(os.path.isdir(os.path.join(images_path, entry))):
+            new_folder_path = os.path.join(images_path, entry)
+        for image in os.listdir(os.path.join(images_path, entry)):
+            if(len(input_data) == max_input_size):
+                return input_data
+            input_data.append(mathplt.image.imread(os.path.join(new_folder_path, image)))
+
+    return input_data
 
 def plot(samples):
-    # Rescale to [0,1] from [-1,1]
     samples = (samples + 1.) / 2.
     fig = plt.figure(figsize=(4, 4))
     gs = gridspec.GridSpec(4, 4)
@@ -27,34 +39,50 @@ def plot(samples):
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_aspect('equal')
-        plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+        plt.imshow(sample)
 
     return fig
 
 class Autoencoder(tf.keras.Model):
-
   def build_encoder(self):
     model = Sequential()
-    model.add(Conv2D(32, (5,5), padding="same", activation="relu"))
-    model.add(Conv2D(64, (5,5), strides=(2,2), activation="relu", padding="same"))
-    model.add(Conv2D(128, (5,5), strides=(2,2), activation="relu", padding="same"))
-    model.add(Conv2D(256, (5,5), strides=(2,2), activation="relu", padding="same"))
-    model.add(Conv2D(512, (5,5), strides=(2,2), activation="relu", padding="same"))
+    model.add(Conv2D(16, (5,5), padding="same"))
+    model.add(LeakyReLU())
+    model.add(MaxPool2D())
+    model.add(Dropout(0.25))
+    model.add(Conv2D(25, (5,5), strides=(1,1), padding="same"))
+    model.add(LeakyReLU()) 
+    model.add(MaxPool2D())
     model.add(Flatten())
+    model.add(Dense(16*16*25))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.25))
+    model.add(Dense(16*16*3))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.25))
     model.add(Dense(self.latent_dim, activation="tanh"))
       
     return model
 
   def build_decoder(self):
     model = Sequential()
-    model.add(Dense(6272))
-    model.add(Reshape((7, 7, 128)))
-    model.add(Conv2D(64, (5,5), activation="relu", padding="same"))
+    model.add(Dense(16*16*3, input_dim=self.latent_dim))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.25))
+    model.add(Dense(16*16*3))
+    model.add(LeakyReLU())
+    model.add(Dense(32*32*25))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.25))
+    model.add(Reshape((32, 32, 25)))
+    model.add(Conv2DTranspose(15, (5,5), padding='same', strides=(1,1)))
+    model.add(LeakyReLU())
     model.add(UpSampling2D())
-    model.add(Conv2D(32, (5,5), activation="relu", padding="same"))
+    model.add(Conv2DTranspose(3, (5,5), padding='same', strides=(1,1)))
+    model.add(LeakyReLU())
     model.add(UpSampling2D())
-    model.add(Conv2D(1, (5,5), activation="tanh", padding="same"))
-      
+    model.add(Conv2D(3, (5,5), padding='same', activation="tanh", strides=(1,1)))
+    
     return model
 
   def __init__(self, latent_dim):
@@ -69,25 +97,23 @@ class Autoencoder(tf.keras.Model):
     return decoded
 
 if __name__ == "__main__":
-    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
-    train_images = (2  * np.array(train_images) / 255) - 1
+    train_images = load_data()
+    train_images = (2  * np.array(train_images)) - 1
 
-    autoencoder = Autoencoder(latent_dim)
+    autoencoder = Autoencoder(128)
     autoencoder.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=0.0002, beta_1=0.5))
-
-    i = 0
     for iterations in range(epochs):
+        print_iteration = True
         batches = data_source.ArrayDataSource([train_images])
         for batch in batches.batch_iterator(batch_size, True):
-            if(i % 100 == 0):
-                encoded_image = autoencoder.encoder(batch[0][0].reshape(-1,28,28,1))
-                print(np.shape(encoded_image))
-                decoded_image = autoencoder.decoder(encoded_image)
-                plt.imshow(np.reshape(decoded_image, (28,28,1)), cmap='Greys_r')
-                plt.savefig('autoencoder/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+            if(print_iteration):
+                encoded_images = autoencoder.encoder(batch[0].reshape(batch_size,128,128,3))
+                decoded_images = autoencoder.decoder(encoded_images)
+                plot(decoded_images)
+                plt.savefig('out/{}.png'.format(str(iterations).zfill(3)), bbox_inches='tight')
                 plt.close()
-            i = i + 1
-            loss = autoencoder.train_on_batch(batch[0].reshape(-1,28,28,1), batch[0].reshape(-1,28,28,1))
-            print(loss)
+                print_iteration = False
+            loss = autoencoder.train_on_batch(batch[0].reshape(-1,128,128,3), batch[0].reshape(-1,128,128,3))
+        print(loss)
         autoencoder.encoder.save_weights("encoder.h5")
         autoencoder.decoder.save_weights("decoder.h5")
